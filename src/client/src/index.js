f387b8d02c0f06cffbe47b5f53f22f3fc0e33923
Greg Bowne <gbowne144@gmail.com>
2023-08-25 19:12:21 -0700

    Adding more files and fixing web-vitals issue

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 
 createRoot(document.getElementById('root')).render(
@@ -9,3 +10,10 @@ createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+serviceWorker.unregister();
