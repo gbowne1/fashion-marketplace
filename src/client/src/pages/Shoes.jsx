a14a960e2f9e627f0d57ecdeec24fbd328098a71
Gregory Bowne <gbowne144@gmail.com>
2025-12-14 19:26:54 -0800

    Update Shoes.jsx

--- src/client/src/pages/Shoes.jsx
+++ src/client/src/pages/Shoes.jsx
@@ -1,3 +1,17 @@
-function Shoes() {}
+import React from 'react';
+import { Typography, Container, Box } from '@mui/material';
 
-export default Shoes;
+export default function Shoes() {
+	return (
+		<Container>
+			<Box sx={{ my: 4 }}>
+				<Typography variant="h4" component="h2" gutterBottom>
+					Shoes
+				</Typography>
+				<Typography variant="body1">
+					Step out in style with our curated footwear collection.
+				</Typography>
+			</Box>
+		</Container>
+	);
+}
