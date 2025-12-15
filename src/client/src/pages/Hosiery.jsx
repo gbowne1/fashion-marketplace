a8b4ce0754b08adb10452c2f75bdb27a8eaaf369
Gregory Bowne <gbowne144@gmail.com>
2025-12-14 19:23:56 -0800

    Update Hosiery.jsx

--- src/client/src/pages/Hosiery.jsx
+++ src/client/src/pages/Hosiery.jsx
@@ -1,5 +1,17 @@
-function Hosiery() {
-	// Component code
-}
+import React from 'react';
+import { Typography, Container, Box } from '@mui/material';
 
-export default Hosiery;
+export default function Hosiery() {
+	return (
+		<Container>
+			<Box sx={{ my: 4 }}>
+				<Typography variant="h4" component="h2" gutterBottom>
+					Hosiery & Stockings
+				</Typography>
+				<Typography variant="body1">
+					Complete your look with our selection of tights, socks, and stockings.
+				</Typography>
+			</Box>
+		</Container>
+	);
+}
