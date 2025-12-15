5546c39e1cc63d1f3535b31302be6d45b2d07323
Gregory Bowne <gbowne144@gmail.com>
2025-12-14 19:19:10 -0800

    Update Lingerie.jsx

--- src/client/src/pages/Lingerie.jsx
+++ src/client/src/pages/Lingerie.jsx
@@ -1,5 +1,18 @@
-function Lingerie() {
-	// Component code
-}
+import React from 'react';
+import { Typography, Container, Box } from '@mui/material';
 
-export default Lingerie;
+export default function Lingerie() {
+	return (
+		<Container>
+			<Box sx={{ my: 4 }}>
+				<Typography variant="h4" component="h2" gutterBottom>
+					Lingerie Collection
+				</Typography>
+				<Typography variant="body1">
+					Browse our exquisite selection of slips, robes, and full lingerie sets.
+				</Typography>
+				{/* Product display components will go here */}
+			</Box>
+		</Container>
+	);
+}
