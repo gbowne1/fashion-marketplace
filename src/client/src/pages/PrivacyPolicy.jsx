fc993e57f048c8f774a0b631e1ba640e30b4bc0e
Gregory Bowne <gbowne144@gmail.com>
2025-12-14 19:31:18 -0800

    Update PrivacyPolicy.jsx

--- src/client/src/pages/PrivacyPolicy.jsx
+++ src/client/src/pages/PrivacyPolicy.jsx
@@ -1,5 +1,22 @@
-const PrivacyPolicy = () => {
-	// return <div>PrivacyPolicy</div>;
-};
+import React from 'react';
+import { Typography, Container, Box } from '@mui/material';
 
-export default PrivacyPolicy;
+export default function PrivacyPolicy() {
+	return (
+		<Container>
+			<Box sx={{ my: 4 }}>
+				<Typography variant="h4" component="h2" gutterBottom>
+					Privacy Policy
+				</Typography>
+				<Typography variant="body1" align="left">
+					**1. Information We Collect:** We collect information you provide directly to us, such as your name, email address, and payment information when you make a purchase.
+					<br /><br />
+					**2. How We Use Information:** We use the information we collect to process transactions, send you marketing communications, and improve our services.
+					<br /><br />
+					**3. Data Security:** We implement various security measures to maintain the safety of your personal information.
+					{/* Add more legal text here */}
+				</Typography>
+			</Box>
+		</Container>
+	);
+}
