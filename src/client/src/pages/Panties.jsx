a3f3d04f5d0369f0fccd19cf7c6e94873a6cadd9
Gregory Bowne <gbowne144@gmail.com>
2025-12-14 19:21:13 -0800

    Update Panties.jsx

--- src/client/src/pages/Panties.jsx
+++ src/client/src/pages/Panties.jsx
@@ -1,5 +1,17 @@
-function Panties() {
-	// Component code
-}
+import React from 'react';
+import { Typography, Container, Box } from '@mui/material';
 
-export default Panties;
+export default function Panties() {
+	return (
+		<Container>
+			<Box sx={{ my: 4 }}>
+				<Typography variant="h4" component="h2" gutterBottom>
+					Panties
+				</Typography>
+				<Typography variant="body1">
+					From thongs to briefs, comfort meets elegance.
+				</Typography>
+			</Box>
+		</Container>
+	);
+}
