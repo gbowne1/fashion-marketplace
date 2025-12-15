d2bb0893ef0bcd5f47cc3f2ce4322d34aedbba04
Gregory Bowne <gbowne144@gmail.com>
2025-12-14 19:29:14 -0800

    Update Best.jsx

--- src/client/src/pages/Best.jsx
+++ src/client/src/pages/Best.jsx
@@ -1,5 +1,17 @@
-function Best() {
-	// Component code
-}
+import React from 'react';
+import { Typography, Container, Box } from '@mui/material';
 
-export default Best;
+export default function Best() {
+	return (
+		<Container>
+			<Box sx={{ my: 4 }}>
+				<Typography variant="h4" component="h2" gutterBottom>
+					Best Sellers
+				</Typography>
+				<Typography variant="body1">
+					See what everyone is loving this season!
+				</Typography>
+			</Box>
+		</Container>
+	);
+}
